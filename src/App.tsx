cfdc0257e93ccab6fbeb3b8e2ba99324d2d6e7b7
Parth Sarin <psarin@stanford.edu>
2022-02-28 22:32:28 -0800

    add google authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import './App.css';
+import { PacmanLoader } from 'react-spinners';
 
 function App() {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    auth.onAuthStateChanged(newUser => {
+      if (newUser) {
+        if (user === null) {
+          setUser(newUser);
+          setLoading(false);
+        }
+      } else {
+        signInWithRedirect(auth, provider);
+      }
+    });
+  })
+
+  const addOn = loading 
+    ? (
+      <div className='fixed w-full h-full flex align-center z-50 bg-black bg-opacity-50'>
+        <div className="flex w-full h-full justify-center">
+          <div className="flex self-center">
+            <PacmanLoader color={'white'} size={100} />
+          </div>
+        </div>
+      </div>
+    )
+    : null;
+
   return (
+    <>
+    { addOn }
     <div className="app flex justify-center h-full align-middle">
-      <h1 className="text-3xl font-bold italic h-fit self-center">
-        Hello from AI Lit!
-      </h1>
+      {
+        user ? (
+          <div className="flex flex-col justify-center h-full align-middle">
+            <h1 className="text-3xl font-bold italic h-fit self-center">
+              Welcome, {user.displayName}!
+            </h1>
+            <img 
+              src={user.photoURL} 
+              alt={user.displayName}
+              className="rounded-full h-20 w-20 self-center mt-2"
+            />
+          </div>
+        ) : null
+      }
     </div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKH_LPp47GMwT8Nwh9S8I2MQTt7J_O3xM",
+  authDomain: "ai-lit-rabbits.firebaseapp.com",
+  projectId: "ai-lit-rabbits",
+  storageBucket: "ai-lit-rabbits.appspot.com",
+  messagingSenderId: "16828976693",
+  appId: "1:16828976693:web:d29182728ec4db7e80ab7c"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <App />,
