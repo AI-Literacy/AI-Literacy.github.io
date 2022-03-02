bfbf72642703f2ca51a7e040131261c3a94d935b
Parth Sarin <psarin@stanford.edu>
2022-03-01 21:04:48 -0800

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,63 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import { User } from '@firebase/auth-types';
 import './App.css';
 import { PacmanLoader } from 'react-spinners';
 import Nav from './Nav';
+import { 
+  HashRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 const UserContext = createContext<User | null>(null);
+
+const Welcome = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div className="app flex justify-center h-5/6 align-middle">
+      {
+        user ? (
+          <div className="flex flex-col justify-center h-full align-middle">
+            <h1 className="text-3xl font-bold italic h-fit self-center">
+              Welcome, {user.displayName}!
+            </h1>
+            <img
+              src={user.photoURL!}
+              alt={user.displayName!}
+              className="rounded-full h-20 w-20 self-center mt-2"
+            />
+          </div>
+        ) : null
+      }
+    </div>
+  );
+}
+
+const NewGame = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div className="app flex justify-center h-5/6 align-middle">
+      {
+        user ? (
+          <div className="flex flex-col justify-center h-full align-middle">
+            <h1 className="text-3xl font-bold italic h-fit self-center">
+              Starting a new game...
+            </h1>
+            <img
+              src={user.photoURL!}
+              alt={user.displayName!}
+              className="rounded-full h-20 w-20 self-center mt-2"
+            />
+          </div>
+        ) : null
+      }
+    </div>
+  );
+}
+
 
 function App() {
   const [user, setUser] = useState<any>(null);
@@ -43,22 +95,12 @@ function App() {
     <UserContext.Provider value={user}>
       { addOn }
       { user ? <Nav /> : null }
-      <div className="app flex justify-center h-5/6 align-middle">
-        {
-          user ? (
-            <div className="flex flex-col justify-center h-full align-middle">
-              <h1 className="text-3xl font-bold italic h-fit self-center">
-                Welcome, {user.displayName}!
-              </h1>
-              <img 
-                src={user.photoURL} 
-                alt={user.displayName}
-                className="rounded-full h-20 w-20 self-center mt-2"
-              />
-            </div>
-          ) : null
-        }
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Welcome />} />
+          <Route path='/new-game' element={<NewGame />} />
+        </Routes>
+      </Router>
     </UserContext.Provider>
   );
 }
