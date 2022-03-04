cbe1a6b2c290ca85e526690f910c0d839096384f
Parth Sarin <psarin@stanford.edu>
2022-03-03 21:57:07 -0800

    responsive design and fix build error

--- src/NewGame/NewGame.tsx
+++ src/NewGame/NewGame.tsx
@@ -12,7 +12,7 @@ const NewGame = () => {
 
     return (
         <div className="app w-4/5 mt-8 mx-auto flex flex-row">
-            <div className="flex flex-col w-1/2">
+            <div className="flex flex-col w-full md:w-1/2">
                 <h1 className="text-4xl mb-8">Create a new game</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
@@ -90,7 +90,7 @@ const NewGame = () => {
                     </div>
                 </form>
             </div>
-            <div className="flex flex-col justify-center items-end">
+            <div className="hidden md:flex md:w-1/2 flex-col justify-center items-end">
                 <img className="w-4/5 mb-6" style={{ maxHeight: '60%' }} src="/img/new-game.svg" alt="" />
             </div>
         </div>
