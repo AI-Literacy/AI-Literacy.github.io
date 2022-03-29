951b47c3a8828032be7d7f85759c11c4ed576d56
Isabel Sieh <isabelsieh@DN51vt9p.SUNet>
2022-03-29 11:18:50 -0700

    logout button

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import UserContext from '../App/UserContext';
+import { getAuth, GoogleAuthProvider, signOut } from 'firebase/auth';
+
 
 const Nav = () => {
     const user = useContext(UserContext);
@@ -9,6 +11,12 @@ const Nav = () => {
         return null;
     }
 
+    function signOutFn(){
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+        auth.signOut();
+    }
+    
     return (
         <nav className="flex justify-between flex-wrap bg-purple-600 p-4">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -20,13 +28,18 @@ const Nav = () => {
                 <div className="flex-grow">
                     {/* Eventually, put some links here */}
                 </div>
-                <div className="flex flex-row items-center hover:cursor-pointer">
+                <div className="flex flex-row items-center ml-5 mr-5 hover:cursor-pointer">
                     <img
                         src={user.photoURL!}
                         alt=""
                         className="rounded-full h-8 w-8 mr-2"
                     />
                     {user.displayName}
+                </div>
+                <div className="flex flex-row items-center ml-5 mr-5 hover:cursor-pointer">
+                    <button onClick={() => signOutFn()}>
+                        {"Logout"}
+                    </button>
                 </div>
             </div>
         </nav>
